b7ad078c5f0452d592e4d2f55bc4f4d55dcde9cb
MaC <mac@Memoona-Ishaqs-MacBook.local>
2025-06-15 03:05:27 +0300

    K-Means Implementation

---
+++ README.md
@@ -0,0 +1,47 @@
+# KMeans
+
+This repository contains a C++ implementation of the K-Means clustering algorithm parallelized using OpenMP. K-Means is a popular unsupervised machine learning algorithm used for clustering data points into a predefined number of clusters. Parallelizing the algorithm using OpenMP allows for significant speedup on multi-core processors.
+
+# Getting Started
+
+## Prerequisites
+This implementation requires the following dependencies:
+
+* C++ compiler with OpenMP support (e.g., g++)
+* The OpenMP library
+
+## Compilation
+1. Clone this repository to your local machine.
+2. Navigate to the repository's directory.
+3. Run `g++ main.cpp -o kmean -fopenmp -march=native`
+
+
+## Usage
+
+### Input
+
+The algorithm takes as input a csv file containing one observation in each row. The feature must be separated by commas. Some inputs generated with `dataset_gen.py` are provided.
+
+### Running the Program
+
+To run use <code> ./kmean <dataset_file_name> <number_of_clusters> <thread_number> <algorithm_type> </code>
+
+- <code><dataset_file_name></code> path of the dataset as a csv file
+- <code><number_of_clusters></code> number of clusters
+- <code><thread_number></code> number of thread to be used
+- <code> <algorithm_type></code>  can be: _rand_ or _pp_ for random or kmeans pp initializer respectivly
+
+For example:
+`./kmean 100000_3_6.csv 6 16 rand`
+
+### Generate datasets
+
+The dataset_gen.py module allows the user to create custom datasets and visualize the result of the kmeans algorithm. To use run `python3 dataset_gen.py`and follow the instructions.
+
+# Results
+The result of the kmean algorithm is shown below
+
+Raw Data                   |  After kmeans
+:-------------------------:|:-------------------------:
+![](images/Screenshot%20from%202023-08-10%2014-14-58.png)   |  ![](images/Screenshot%20from%202023-08-10%2014-16-05.png)
+
---
+++ main.cpp
@@ -0,0 +1,77 @@
+
+#include "kmeans.cpp"
+#include "initializer.cpp"
+#include "Point.h"
+#include "utils.cpp"
+
+// Compile with:
+// g++ main.cpp -o kmean -fopenmp
+
+#define TEST 0
+
+int release(int argc, const char *argv[]) {
+    
+    std::string file_name = argv[1];
+    int clusters = atoi(argv[2]);
+    int threads = atoi(argv[3]);
+    std::string algorithm = argv[4];
+    int epochs = 1000;
+    double dtime;
+    std::vector<Point> points = load_csv(file_name);
+    
+
+    if (algorithm == "rand"){
+        dtime = omp_get_wtime();
+        std::vector<Point> centroids = random_initializer(points, clusters);
+        centroids = kmean_par (points, centroids, epochs, clusters, threads);
+        dtime = omp_get_wtime() - dtime;
+        std::cout<< dtime<<std::endl;
+    }
+    else if (algorithm == "pp"){
+        dtime = omp_get_wtime();
+        std::vector<Point> centroids = kmeanpp_initializer(points, clusters, threads);
+        centroids = kmean_par (points, centroids, epochs, clusters, threads);
+        dtime = omp_get_wtime() - dtime;
+        std::cout<< dtime<<std::endl;
+    }
+    else{
+        std::cout<<"Invalid algorithm"<<std::endl;
+        return 1;
+    }
+    output_results(points);
+    return 0;
+}
+
+int test(int argc, const char *argv[]) {
+    std::string file_name = argv[1];
+    int clusters = atoi(argv[2]);
+    int threads = atoi(argv[3]);
+    std::string algorithm = argv[4];
+    int epochs = 1000;
+    double dtime;
+    std::vector<Point> points = load_csv(file_name);
+
+    if (algorithm == "rand"){
+        dtime = omp_get_wtime();
+        std::vector<Point> centroids = random_initializer(points, clusters);
+        dtime = omp_get_wtime() - dtime;
+        std::cout<< dtime<<std::endl;
+    }
+    else if (algorithm == "pp"){
+        dtime = omp_get_wtime();
+        std::vector<Point> centroids = kmeanpp_initializer(points, clusters, threads);
+        dtime = omp_get_wtime() - dtime;
+        std::cout<< dtime<<std::endl;
+    }
+
+    
+    return 0;
+}
+
+int main(int argc, const char *argv[]) {
+#if TEST
+    return test(argc, argv);
+#else
+    return release(argc, argv);
+#endif
+}
